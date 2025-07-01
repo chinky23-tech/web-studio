429d8f34d52d95e86dec92e2b779bc46c49087db
chinky23-tech <chinky0330@gmail.com>
2025-07-01 22:49:24 +0530

    changed gradient

--- js/script.js
+++ js/script.js
@@ -6,15 +6,4 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
 });
 
-//color changes hero section scroll up down
-/*document.addEventListener('DOMContentLoaded', function () {
-  const hero = document.querySelector('.hero');
 
-  window.addEventListener('scroll', function () {
-    const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
-    const hue = (scrollPercentage * 3.6) % 360;
-
-    hero.style.backgroundImage = `linear-gradient(135deg, hsl(${hue}, 100%, 70%), hsl(${(hue + 30) % 360}, 100%, 70%))`;
-  });
-});
-*/
